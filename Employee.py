e0a79ce0d27c0d48461ec92e9f3ce0eaae89311f
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-01-20 00:06:07 +0300

    Create Employee.py

---
+++ Employee.py
@@ -0,0 +1,8 @@
+salary = float(input("Enter your salary: "))
+year_of_service = int(input("Enter your year of service: "))
+
+if year_of_service > 5:
+    bonus = salary * 0.05
+    print("Your bonus amount is: $" + str(bonus))
+else:
+    print("You are not eligible for a bonus.")
