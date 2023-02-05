4c77b59ca34d4c3dcb68280e4ce1330707ede4e8
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-05 06:08:41 +0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -8,3 +8,14 @@ for i in range(4,0,-1):
     print("")
 
     # t0 geuss the game
+import random
+
+number = random.randint(1,9)
+guess = 0
+while guess != number:
+    guess = int(input("Enter a guess between 1 and 9: "))
+    if guess > number:
+        print("Too high.")
+    elif guess < number:
+        print("Too low.")
+print("Well guessed!")
