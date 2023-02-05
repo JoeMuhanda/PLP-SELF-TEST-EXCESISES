15ae6aa6bd85df73c69395d8765182b2a3e6d8b2
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-05 06:14:11 +0300

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -41,3 +41,20 @@ for num in num_list:
 
 # Part 8
 print(count)
+
+
+# Part II
+# Program to guess a number between 1 to 9
+
+def guess_number():
+  target_number = 5
+  while True:
+    try:
+      user_guess = int(input("Enter a guess between 1 and 9: "))
+      if user_guess == target_number:
+        print("Well guessed!")
+        break
+    except ValueError:
+      print("Invalid input. Enter a number between 1 and 9.")
+
+guess_number()
