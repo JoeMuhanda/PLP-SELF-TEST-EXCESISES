c535360d34faa7845db3f7c0f33ff8a0c4839ed5
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-11 13:11:35 +0300

    Update user_inputs.py

--- user_inputs.py
+++ user_inputs.py
@@ -1,1 +1,6 @@
 
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
