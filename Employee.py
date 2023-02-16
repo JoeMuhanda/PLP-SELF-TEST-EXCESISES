a981fe063b5ffbbca0054473f1642a3ce4534c63
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-16 21:40:03 +0300

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -5,4 +5,4 @@ if year_of_service > 5:
     bonus = salary * 0.05
     print("Your bonus amount is: $" + str(bonus))
 else:
-    print("You are not eligible for a bonus.")
+    print("You are not eligible for a bonus
