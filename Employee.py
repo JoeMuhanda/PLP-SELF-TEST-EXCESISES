86297dcb4bd3be25c25c0c330ca44301e943cfd3
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-16 21:41:52 +0300

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -1,7 +1,7 @@
 salary = float(input("Enter your salary: "))
 year_of_service = int(input("Enter your year of service: "))
 
-if year_of_service > 5:.")
+if year_of_service > 5:.
 
     bonus = salary * 0.05
     print("Your bonus amount is: $" + str(bonus))
