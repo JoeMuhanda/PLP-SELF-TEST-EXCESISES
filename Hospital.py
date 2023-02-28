220f8525f0539f42af0f938f74a5e6d9cdbe3e26
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-03-01 00:46:52 +0300

    Update Hospital.py

--- Hospital.py
+++ Hospital.py
@@ -1,4 +1,8 @@
 
+ # Declare variables to store patient information
+patient_name =input( "Patient name" )
+patient_age = input ("patient age")
+patient_status = input ("Patient status")
 
 # Print patient check-in information
 print("Patient Name: ", patient_name)
