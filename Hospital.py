d7c4052f40aebd7ab6102b6159c2ca291f87fa5b
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-03-01 00:46:16 +0300

    Update Hospital.py

--- Hospital.py
+++ Hospital.py
@@ -3,7 +3,4 @@ patient_name =input( "Patient name" )
 patient_age = input ("patient age")
 patient_status = input ("Patient status")
 
-# Print patient check-in information
-print("Patient Name: ", patient_name)
-print("Patient Age: ", patient_age
-print("Patient Status: ", patient_status)
+
