b2f72bd758b8efa29f480157b59870710ea73484
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-03-01 00:47:43 +0300

    Update Hospital.py

--- Hospital.py
+++ Hospital.py
@@ -1,5 +1,4 @@
-
-    # Declare variables to store patient information
+# Declare variables to store patient information
 patient_name =input( "Patient name" )
 patient_age = input ("patient age")
 patient_status = input ("Patient status")
