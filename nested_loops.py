ee38c79dd10b6a377e5a35412c1b395311860959
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-04-15 00:02:07 +0300

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -1,4 +1,19 @@
+# Part 1
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
 
+for num in num_list:
+  print(num)
+
+# Part 2
+for num in num_list:
+  if num > 45:
+    print(num)
+
+# Part 3
+for num in num_list:
+  if num > 45:
+    print("Over 45")
+  else:
     print("Under 45")
 
 # Part 4
